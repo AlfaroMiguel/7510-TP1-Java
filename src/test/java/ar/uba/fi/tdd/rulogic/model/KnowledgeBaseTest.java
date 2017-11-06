142477143cd9affe38c0b52be6c1f2f8fba1e901
AlfaroMiguel <miguel.alfaro.a95@gmail.com>
2017-11-06 04:44:26 -0300

    Add Test

--- src/test/java/ar/uba/fi/tdd/rulogic/model/KnowledgeBaseTest.java
+++ src/test/java/ar/uba/fi/tdd/rulogic/model/KnowledgeBaseTest.java
@@ -15,13 +15,72 @@ public class KnowledgeBaseTest {
 	@Before
 	public void setUp() throws Exception {
 		initMocks(this);
+		knowledgeBase.parseDB("src/main/resources/rules.db");
 	}
 
 	@Test
-	public void test() {
+	public void testTrueFact() {
+		Assert.assertTrue(this.knowledgeBase.answer("varon(juan)."));
+	}
 
-		Assert.assertTrue(this.knowledgeBase.answer("varon (javier)."));
+	@Test
+	public void testTrueFactWithoutWhiteSpace() {
+		Assert.assertTrue(this.knowledgeBase.answer("mujer(maria)."));
+	}
 
+	@Test
+	public void testTrueFactWithWhitepace() {
+		Assert.assertTrue(this.knowledgeBase.answer("mujer (maria) ."));
+	}
+
+	@Test
+	public void testFalseFact() {
+		Assert.assertFalse(this.knowledgeBase.answer("varon (javier)."));
+	}
+
+	@Test
+	public void testTrueFactWithTwoArgs() {
+		Assert.assertTrue(this.knowledgeBase.answer("padre(juan, pepe)."));
+	}
+
+	@Test
+	public void testTrueFactWithTwoArgsAndWhitespaces() {
+		Assert.assertTrue(this.knowledgeBase.answer("padre (juan,   pepe)   ."));
+	}
+
+	@Test
+	public void testFalseFactWithTwoArgs() {
+		Assert.assertFalse(this.knowledgeBase.answer("padre (juan, miguel)."));
+	}
+
+	@Test
+	public void testTrueRuleWithTwoArgs() {
+		Assert.assertTrue(this.knowledgeBase.answer("hijo(pepe,juan)."));
+	}
+
+	@Test
+	public void testFalseRuleWithTwoArgs() {
+		Assert.assertFalse(this.knowledgeBase.answer("hijo(juan,pepe)."));
+	}
+
+	@Test
+	public void testTrueRuleWithThreeArgs() {
+		Assert.assertTrue(this.knowledgeBase.answer("tio(nicolas, cecilia, roberto)."));
+	}
+
+	@Test
+	public void testFalseRuleWithThreeArgs() {
+		Assert.assertFalse(this.knowledgeBase.answer("tio(pepe, cecilia, roberto)."));
+	}
+
+	@Test
+	public void testIncompleteQueryIsFalse() {
+		Assert.assertFalse(this.knowledgeBase.answer("tio(pepe, cecil"));
+	}
+
+	@Test
+	public void testNonExistingRuleIsFalse() {
+		Assert.assertFalse(this.knowledgeBase.answer("sobrino(pepe, cecilia)"));
 	}
 
 }
